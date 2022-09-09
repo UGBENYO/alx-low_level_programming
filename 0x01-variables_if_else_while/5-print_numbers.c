a9419a308d81b10792f653eb4a13be399154652e
“UGBENYO” <ugbenyoj@gmail.com>
2022-09-09 14:21:23 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * return: Always 0 (success/correct)
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+
+	{
+		printf("%i", i);
+	}
+
+	printf("\n");
+
+	reurn (0);
+}
