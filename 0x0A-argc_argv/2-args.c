f3120473eb8e4e01c5676875c746d88a4d231926
“UGBENYO” <ugbenyoj@gmail.com>
2022-09-30 11:58:47 -0700

    agrc tasks

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main: Entry point
+ * main - Entry point
  * @argc: arguments
  * @argv: array pointing to arguments
  * Return: 0
@@ -10,8 +10,8 @@
 
 int main(int argc, char *argv[])
 {
-	int i  = 0;w
-		
+	int i  = 0;
+
 	while (i < argc)
 	{
 		printf("%s\n", argv[i]);
