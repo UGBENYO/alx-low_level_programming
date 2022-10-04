6aefa02ce1f002d4e96ba3df258518d14bd401c7
“UGBENYO” <ugbenyoj@gmail.com>
2022-10-04 04:24:19 -0700

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * create_array - creates an array and initializes with specific char
@@ -11,7 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	char *arr;
-	unsigned int;
+	unsigned int i;
 
 	if (size == 0)
 	{
