78bb5b73a52b1a4f1da065d1012248dd1491462b
“UGBENYO” <ugbenyoj@gmail.com>
2022-12-08 19:27:32 -0800

    double linked tasks

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,7 +2,7 @@
 /**
  * print_dlistint - prints all the elements of a
  * dlistint_t list
- * @head: head of the list
+ * @h: head of the list
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,7 +2,7 @@
 /**
  * dlistint_len - returns the number of elements in
  * a double linked list
- * @head: head of list
+ * @h: head of list
  * Return: number of nodes
  */
 size_t dlistint_len(const dlistint_t *h)
